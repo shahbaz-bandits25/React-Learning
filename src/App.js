f1fedaaea7617557cc3d7ab622a94914a7fc29fa
shahbaz-bandits25 <shahbaz_djrian@hotmail.com>
2024-09-30 21:58:34 +0500

    Video 16 code which includes how to get input box val and hide and display on click in functional component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Admin from './Admin';
 import Test from './video-11';
 import StateManagementLectrue from './video-12';
 import StateManagementLectrueInClassComponent from './video-13';
+import GetInputBoxValue from "./video-16";
 
 function App() {
     const [email,setEmail] = useState('azfar@gmail.com');
@@ -20,14 +21,16 @@ function App() {
     <Test/> */}
         {/* <StateManagementLectrue/> */}
         {/*<StateManagementLectrueInClassComponent/>*/}
-        <Student name={'Shahbaz'} email={'shahbaz@gmail.com'} others={{city:'Karachi',country:'Pakistan'}} />
-        <Student name={'Faisal'} email={'faisal@gmail.com'} others={{city:'Lahore',country:'Pakistan'}}/>
-        <Student name={'Arooba'} email={'arooba@gmail.com'} others={{city:'Frankfurt',country:'Germany'}} />
+    {/*    <Student name={'Shahbaz'} email={'shahbaz@gmail.com'} others={{city:'Karachi',country:'Pakistan'}} />*/}
+    {/*    <Student name={'Faisal'} email={'faisal@gmail.com'} others={{city:'Lahore',country:'Pakistan'}}/>*/}
+    {/*    <Student name={'Arooba'} email={'arooba@gmail.com'} others={{city:'Frankfurt',country:'Germany'}} />*/}
 
-    {/*    Here I'm Passing the state as Prop to the Student component*/}
-        <Student name={'Azfar'} email={email} others={{city:city,country:'Pakistan'}} />
-        <button onClick={()=>{setEmail('abc@gmail.com')}}>Update Email</button>
-        <button onClick={()=>{setCity('Islamabad')}}>Update City</button>
+    {/*/!*    Here I'm Passing the state as Prop to the Student component*!/*/}
+    {/*    <Student name={'Azfar'} email={email} others={{city:city,country:'Pakistan'}} />*/}
+    {/*    <button onClick={()=>{setEmail('abc@gmail.com')}}>Update Email</button>*/}
+    {/*    <button onClick={()=>{setCity('Islamabad')}}>Update City</button>*/}
+
+        <GetInputBoxValue />
     </div>
   );
 }
